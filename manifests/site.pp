9f686807a828e883817dc332a5dd796bc884a9a9
Cosmin Ioan <cioan@pih.org>
2014-11-12 08:46:39 -0500

    install IMB newrelic agent

--- manifests/site.pp
+++ manifests/site.pp
@@ -3,7 +3,6 @@ Exec { path => [ '/bin/', '/sbin/' , '/usr/bin/', '/usr/sbin/', '/usr/local/bin/
 notify{"hostname= ${hostname}": }
 
 node default {
-  
-  include pih_java  
+    
   include newrelic
 }
